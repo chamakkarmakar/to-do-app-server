bac6b98c0ae07a9939bd9811f8319f477463aa67
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-05-19 03:20:31 +0600

    load data from mongodb

--- index.js
+++ index.js
@@ -2,9 +2,39 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 app.use(cors())
 app.use(express.json())
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hmmar.mongodb.net/?retryWrites=true&w=majority`;
+const uri = 'mongodb+srv://dbUser2:cMWtGSOgDB2z4UxK@cluster0.hmmar.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Mongodb Connected');
+        const taskCollection = client.db("dbTaskManage").collection("tasks");
+
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+            
+        })
+        
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('TO Do App');
